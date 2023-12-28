4b787f1e86a6352b7ecd11d39297d6c1c78455ce
Jazeye <jaseelp394@gmail.com>
2023-12-28 20:35:59 +0530

    fun,obj,array,method

--- javascript.js
+++ javascript.js
@@ -1,1 +1,83 @@
-console.log("hai")
+//function calling
+
+// function myFun(){
+//     let a = 10;
+//     myFun2();
+//     return a;
+// }
+
+// function myFun3(){
+//     let a = 11;
+//     myFun2();
+//     return a;
+// }
+// function myFun2(){
+//     console.log("completed")
+   
+// } 
+//  console.log(myFun());    
+//  console.log(myFun3());   
+
+//function calling end
+
+//parameter passing 
+// function myFun(id){
+//     let a = 10;
+//     let b = id;
+//     let c = a+b;
+//     return c;
+// }
+// console.log(myFun(10))
+//parameter passing end
+
+// // only one value not using return 
+//  myfunc = (c) => c;
+//  console.log(myfunc(11));
+
+// // or one parameter only not using ()
+// myfunc = c => c;
+// console.log(myfunc(551));
+
+// alert ("blocked");
+
+// //object
+
+// let user = {
+//     firstName: "john",
+//     lastName: "evi"
+// };
+// console.log(user.firstName);
+// console.log(user);
+// //object end
+
+// //Array
+
+// let array =["test","tes"]
+// console.log(array[0]);
+
+// //arrya end
+
+// str method
+
+// let a = "Minnal op" //white space include
+// console.log(a.length);
+
+
+// indexof 
+// let a = "Minnal op" //white space include & start with 0
+// console.log(a.indexOf("M"));
+
+// console.log(a.lastIndexOf("n")); //choose ;last index value
+
+// search
+// let a = "Minnal op" //start with 0
+// console.log(a.search("l"));
+
+// //include
+// let a = "Minnal op" //out put based on true or false
+// console.log(a.includes("l"));
+
+// slice
+let a = "Minnal op" //start with 0
+let b = a.slice(0,10)
+console.log(b);
